d42462144b15ba99276f1a915c5eeeea6e1bce80
Sathish John <sjohn@ncsu.edu>
2012-06-10 21:26:43 -0400

    initial checkin

---
+++ sampleRail/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SampleRail::Application.config.secret_token = 'fc58475eeaf163363eba414a6d462094e7e730540671ed8f2d71a42ea75f7a59927b968c69dede69cc288fe35f665d0e52a619946e08373a6dc8798a14ce2b87'
